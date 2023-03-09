f13c74373e511d6a28e69c8a123ffbba4383c0dd
HazmatBP <hazmatbp@gmail.com>
2023-03-09 12:36:48 +1100

    Fixed some horrible formatting in the print statement

--- CLI_Version.py
+++ CLI_Version.py
@@ -3,9 +3,10 @@ print("Welcome to the Health App!  \nPress any key to start:")
 key = readchar.readkey()
 
 if key:
-    print("What do you want to do?",
-          "Press 1 to add new activity\n",
-          "Press 2 to view stats\n",
-          "Press 3 to exit\n")s
+    print("""What do you want to do?
+Press 1 to add new activity:
+Press 2 to view stats:
+Press 3 to exit:\n""")
+    
 else:
     exit()
