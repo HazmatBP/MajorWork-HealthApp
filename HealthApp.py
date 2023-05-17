552085ca1b981539856c052206bfb685dd4f8c39
HazmatBP <hazmatbp@gmail.com>
2023-05-17 13:01:47 +1000

    HORRIBLE LAMBDA SHIT WITH READING

--- HealthApp.py
+++ HealthApp.py
@@ -2,23 +2,31 @@ import ttkbootstrap as ttk
 from datetime import *
 
 
-def save_to_file(filename):
+def save_with_date(filename, user_input):
     # get user input from entry widget
-    user_input = entry.get()
-
+    print(user_input)
     # get current date
-    current_date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    current_date = datetime.now().strftime('%d%m%Y')
 
     # write user input and current date to file
     with open(filename, 'a') as file:
-        file.write(f'{current_date} - {user_input}\n')
+        file.write(f'{current_date}-{user_input}\n')
 
-    # clear the text box
-    entry.delete(0, 'end')
 
-def read_dateinfo_from_file(filename, dictionary):
+def read_dateinfo_from_file(file_path):
+    date_dict = {}
     
-    
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        split_line = [] # temporary array used to split the datestamp and value into seperate values  
+        
+        for i in range(len(lines)):
+            split_line = lines[i].split("-") 
+            date_dict.update({split_line[0], split_line[1]}) # adds a key:value pair to the dictionary, with the datestamp as the key and the value as the value
+     
+    file_path.close()  
+    return date_dict
+
     
 def reset_file(filename):
     # delete everything from the file
@@ -28,7 +36,7 @@ def reset_file(filename):
 def save_on_key_press(event):
     # check if 'enter' key was pressed
     if event.keysym == 'Return':
-        save_to_file("saved_data.txt")
+        save_with_date("saved_data.txt")
 
 # create main tkinter window
 app = ttk.Window(themename = 'darkly')
@@ -44,7 +52,9 @@ input_frame = ttk.Labelframe(app, text = "Input")
 entry = ttk.Entry(input_frame)
 entry.pack(side = 'left', padx = 5, pady = 5)
 
-save_button = ttk.Button(input_frame, text='Save', command = lambda: save_to_file("saved_data.txt"))
+
+save_button = ttk.Button(input_frame, text='Save', command = lambda : (save_with_date("saved_data.txt", entry.get())))
+
 save_button.pack(side = 'left', padx = 5, pady = 5)
 
 reset_button = ttk.Button(input_frame, text = 'Reset', command = lambda: reset_file("saved_data.txt"))
@@ -54,6 +64,8 @@ input_frame.pack(padx= 5, pady = 5)
 
 # bind 'enter' key to save_to_file function
 entry.bind('<KeyPress>', save_on_key_press)
+
+
 
 
 # create output field
