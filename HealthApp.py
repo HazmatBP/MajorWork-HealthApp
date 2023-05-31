d645aaf6fa2ac1e98b974a2c7dcca95cc9feb769
HazmatBP <hazmatbp@gmail.com>
2023-05-31 12:56:31 +1000

    Started converting the program to use dicts instead of files

--- HealthApp.py
+++ HealthApp.py
@@ -1,19 +1,62 @@
 import ttkbootstrap as ttk
 from datetime import *
 
+#! currently unused file saving functions, need to be rewritten to work when the program is started/closed
+# def save_steps_with_date(filename, entry_widget):
+#     # get user input from entry widget
+#     user_input = entry_widget.get()
+    
+#     if user_input.isnumeric(): #only saves the data if the input is a number.
+        
+#         user_input = int(user_input)
+#         print(user_input)
+#         # get current date
+#         current_date = datetime.now().strftime('%d%m%Y')
 
-def save_with_date(filename, entry_widget):
+#         # write user input and current date to file
+#         with open(filename, 'a') as file:
+#             file.write(f'{current_date}-{user_input}\n')
+            
+#         entry_widget.delete(0, "end")
+
+# def read_dateinfo_from_file(file_path):
+#     date_dict = {}
+    
+#     with open(file_path, "r") as file:
+#         lines = file.readlines()
+#         split_line = [] # temporary array used to split the datestamp and value into seperate values  
+        
+#         for i in range(len(lines)):
+#             split_line = lines[i].split("-") 
+#             date_dict.update({split_line[0], split_line[1]}) # adds a key:value pair to the dictionary, with the datestamp as the key and the value as the value
+            
+#     file_path.close()  
+#     return date_dict
+
+def save_with_date(filename, entry_widget, dictionary):
     # get user input from entry widget
     user_input = entry_widget.get()
-    print(user_input)
-    # get current date
-    current_date = datetime.now().strftime('%d%m%Y')
-
-    # write user input and current date to file
-    with open(filename, 'a') as file:
-        file.write(f'{current_date}-{user_input}\n')
+    
+    if user_input.isnumeric(): #only saves the data if the input is a number.
         
-    entry_widget.delete(0, "end")
+        user_input = int(user_input)
+        print(user_input)
+        # get current date
+        current_date = datetime.now().strftime('%d%m%Y')
+
+
+        # If the date being written to is already in the dictionary, this if statement will add the new value to the existing value instead of overwriting it.
+        if current_date in dictionary: 
+            existing_value = dictionary.get(current_date)
+            new_value = existing_value + user_input
+            
+            dictionary.update({current_date: new_value})
+            
+        else:
+            dictionary.update({current_date: user_input})
+        
+            
+        entry_widget.delete(0, "end")
 
 
 def read_dateinfo_from_file(file_path):
@@ -41,6 +84,15 @@ def save_on_key_press(event):
     if event.keysym == 'Return':
         save_with_date("saved_data.txt", entry)
 
+
+#! defining variables
+steps_dict = {}
+
+
+
+
+
+
 # create main tkinter window
 app = ttk.Window(themename = 'darkly')
 
@@ -56,7 +108,7 @@ entry = ttk.Entry(input_frame)
 entry.pack(side = 'left', padx = 5, pady = 5)
 
 
-save_button = ttk.Button(input_frame, text='Save', command = lambda : (save_with_date("saved_data.txt", entry.get(), entry)))
+save_button = ttk.Button(input_frame, text='Save', command = lambda : save_with_date("saved_data.txt", entry, steps_dict))
 
 save_button.pack(side = 'left', padx = 5, pady = 5)
 
@@ -81,3 +133,8 @@ output_frame.pack(padx= 5, pady = 5)
 
 # start main loop
 app.mainloop()
+
+
+
+
+
