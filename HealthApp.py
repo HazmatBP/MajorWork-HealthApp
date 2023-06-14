1f10edc5834f7955d38dfe2dad4bf7048393e94a
HazmatBP <hazmatbp@gmail.com>
2023-06-14 12:56:06 +1000

    radiobutton now defines whether the date saved is the current or selected - depending on what value in the radiobutton is selected, a different date will be used - defaults to current date - still need to do widget positioning

--- HealthApp.py
+++ HealthApp.py
@@ -4,20 +4,20 @@
 
 import ttkbootstrap as ttk
 from datetime import *
-import json 
-import os 
+import json
+import os
 
 
 appRunning = True   # variable is always true while the app is running
 
 
 def save_on_closing():
-    
+    global appRunning
     print("Closing the program...")
-    
+
     # writes the contents of the steps dictionary to a .json file
     json.dump(steps_dict, open( "saved_data.json", 'w' ))
-    
+
     # closes the window
     appRunning = False
     app.destroy()
@@ -40,70 +40,79 @@ def load_json_to_dict(file_path):
         output_dict = {}
         with open(file_path, "w") as file:
             json.dump(output_dict, file)
-            
+
     return output_dict
 
-    
+
 
 
 def get_date():
-    
+
     selected_date = date_selector.entry.get()   #? weird internal object magic, is basically just get_date() except it actually works
 
     # Parse the input string as a datetime object
     date_obj = datetime.strptime(selected_date, '%d/%m/%Y')
-    
+
     # Format the datetime object as "YYYYMMDD" string   #? this uses this horribly unreadable date formatting for easy dictionary sorting later
-    
     formatted_date = date_obj.strftime('%Y%m%d')
-    
-    return formatted_date
+
+    # check which radio button is selected
+    date_choice = date_radio_var.get()
+
+    # return either the date from the selector or todays date depending on radiobutton choice
+    if date_choice == "current_date":
+        # return date from selector
+        return formatted_date
+    else:
+        # return todays date 
+        return datetime.today().strftime('%Y%m%d')
 
 def save_with_date(entry_widget, dictionary):
-    
+
     # get user input from entry widget
     user_input = entry_widget.get()
-    
+
     if user_input.isnumeric(): #only saves the data if the input is a number.
-        
+
         user_input = int(user_input)
-        print(user_input)
+        print(user_input) #todo dev print statement
         # get current date
         current_date = get_date()
-        
 
-        
+
+
 
         # If the date being written to is already in the dictionary, this if statement will add the new value to the existing value instead of overwriting it.
-        if current_date in dictionary: 
+        if current_date in dictionary:
             existing_value = dictionary.get(current_date)
             new_value = existing_value + user_input
-            
+
             dictionary.update({current_date: new_value})
-            
+
         else:
             dictionary.update({current_date: user_input})
-            
-        print(dictionary)
-        
+
+        print(dictionary) #todo dev print statement
+
         entry_widget.delete(0, "end") # clear the entry box
 
- 
-    
+
+
 def reset_day(dictionary):
 
     # replaces today's current value with 0
     current_date = datetime.now().strftime('%d%m%Y')
     dictionary.update({current_date: 0})
-    
+    print(dictionary) #todo dev print statement
+
 def save_on_key_press(event):
-    
+
     # check if 'enter' key was pressed
     if event.keysym == 'Return':
         save_with_date(steps_entry, steps_dict)
 
 
-#! creating widgets 
+#! creating widgets
 
 
 # create main tkinter window
@@ -118,7 +127,7 @@ app.protocol("WM_DELETE_WINDOW", save_on_closing)
 title = ttk.Label(app, text='Health App', font = 'Calibri 16 bold')
 title.pack(padx = 10, pady = 5)
 
-# create input frame 
+# create input frame
 input_frame = ttk.Labelframe(app, text = "Steps Recorder")
 
 # create date radiobutton
@@ -130,7 +139,7 @@ date_radio_button2 = ttk.Radiobutton(input_frame, text="Select Date:", variable 
 date_radio_button1.pack(padx = 5, pady = 5)
 date_radio_button2.pack(padx = 5, pady = 5)
 
-# create date selector 
+# create date selector
 date_selector = ttk.DateEntry(input_frame)
 date_selector.pack(side = 'left', padx = 5, pady = 5)
 
@@ -152,9 +161,6 @@ input_frame.pack(padx= 5, pady = 5)
 # bind 'enter' key to save_to_file function
 steps_entry.bind('<KeyPress>', save_on_key_press)
 
-
-
-
 # create output field
 output_frame = ttk.Labelframe(app, text = "Stats Output")
 
@@ -172,7 +178,7 @@ steps_dict = load_json_to_dict("saved_data.json")
 
 while appRunning:
     app.update()
-    
+
 app.destroy()
 
 
